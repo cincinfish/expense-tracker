f2b8a81aeeddb93d2619df33fb9bbd5d296688bc
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-06-25 14:51:46 +0800

    feat: add edit page & function

--- routes/modules/record.js
+++ routes/modules/record.js
@@ -4,7 +4,7 @@ const Category = require('../../models/category')
 const Record = require('../../models/record')
 const checkDate = require('../../public/javascript/checkDate')
 const isFuture = require('../../public/javascript/isFuture')
-
+const moment = require('moment')
 // create
 router.get('/new', async (req, res) => {
   const categories = await Category.find()
@@ -36,6 +36,56 @@ router.post('/', async (req, res) => {
   res.redirect('/')
 })
 
+//edit 
+router.get('/:id/edit', async (req, res) => {
+  const _id = req.params.id
+  const categoryData = await Category.find().lean()
+  return Record.findOne({ _id })
+    .lean()
+    .then((record) => {
+      categoryData.filter(category => {
+        if (JSON.stringify(category._id) === JSON.stringify(record.categoryId)) {
+          record.category = category.name
+          return record.category
+        }
+      })
+      record.date = moment(record.date).format('YYYY-MM-DD')
+      return record
+    }).then((record) => {
+      res.render('edit', { record, categories: categoryData })
+    })
+    .catch(error => {
+      console.log(error)
+      res.render('edit', { error: error.message })
+    })
+})
 
+router.put('/:id', async (req, res) => {
+  const _id = req.params.id
+  const { name, date, categoryId, amount } = req.body
+  const categories = await Category.find().lean()
+  const errors = []
+  if (!name || !date || !categoryId || !amount) {
+    errors.push({ message: '所有欄位都是必填' })
+  }
+  if (checkDate(date) === false) {
+    errors.push({ message: '請輸入合法日期' })
+  }
+  if (isFuture(date) === true) {
+    errors.push({ message: '請勿輸入未來日期' })
+  }
+  if (errors.length) {
+    return res.render('new', {
+      errors, name, date, categoryId, amount, categories
+    })
+  }
+
+  Record.findByIdAndUpdate(_id, req.body)
+    .then(() => res.redirect('/'))
+    .catch(error => {
+      console.log(error)
+      res.render('/', { error: error.message })
+    })
+})
 
 module.exports = router
