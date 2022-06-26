36becee218499ad1a1ea817a713238e1a578772c
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-06-26 12:31:10 +0800

    feat: add add express-session & user login, register flash

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
+// require packages used in the project
 const express = require('express')
 const app = express()
-const methodOverride = require('method-override')
+// require express-handlebars
 const exphbs = require('express-handlebars')
-const routes = require('./routes')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const session = require("express-session")
+const routes = require('./routes')
+const usePassport = require('./config/passport')
+const flash = require('connect-flash')
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const port = process.env.PORT
-
 require('./config/mongoose')
-
-app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs', 
-}))
+// setting template engine
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// setting static files
 app.use(express.static('public'))
-app.use(methodOverride('_method'))
-//app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.danger_msg = req.flash('danger_msg')
+  next()
+})
 app.use(routes)
 
+// start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
+const passport = require('passport')
 
 // login
 router.get('/login', (req, res) => {
   res.render('login')
 })
-router.post('/login', (req, res) => {
-
-})
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 // register
 router.get('/register', (req, res) => {
@@ -16,10 +18,23 @@ router.get('/register', (req, res) => {
 })
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-
+  const errors = []
+  if (!email || !password || !confirmPassword) {
+    errors.push({ msg: 'Email and password are required!' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({
+      msg: 'Password and confirm password does not match!'
+    })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors, name, email, password, confirmPassword
+    })
+  }
   User.findOne({ email }).then(user => {
     if (user) {
-
+      errors.push({ msg: 'User already exists.' })
       return res.render('register', {
         errors,
         name,
@@ -38,4 +53,5 @@ router.post('/register', (req, res) => {
   })
     .catch(err => console.log(err))
 })
+
 module.exports = router
