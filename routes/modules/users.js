6c198df4514cf8658e9a194a78d57a8ddbcc2a66
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-06-26 11:26:57 +0800

    feat: add user register route

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
+
+// login
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -7,7 +10,32 @@ router.post('/login', (req, res) => {
 
 })
 
+// register
 router.get('/register', (req, res) => {
   res.render('register')
+})
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+
+  User.findOne({ email }).then(user => {
+    if (user) {
+
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return User.create({
+      name,
+      email,
+      password
+    })
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
+    .catch(err => console.log(err))
 })
 module.exports = router
