85b4519247dd93d33be322550deb832d9ceb7d8a
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-06-28 01:45:13 +1000

    fix: add node start & add date localeDateString()

--- public/javascript/isFuture.js
+++ public/javascript/isFuture.js
@@ -1,6 +1,6 @@
 function isFuture(date) {
   let selectedDate = new Date(date.replace(/-/g, '-'))
-  let today = new Date()
+  let today = new Date().toLocaleDateString()
   if (selectedDate > today) {
     return true
   }
