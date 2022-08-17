6cafbd0e267c44b80b4da6db6ec144f395c5b820
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-08-17 10:19:25 +0800

    feat: add year select

--- controller/home-controller.js
+++ controller/home-controller.js
@@ -1,33 +1,67 @@
 const Category = require('../models/category')
 const Record = require('../models/record')
 const moment = require('moment')
+const mongoose = require('mongoose')
 
 const homeController = {
   getRecord: async (req, res, next) => {
     try {
       const userId = req.user._id
-      const categoryId = req.query.categorySort ? req.query.categorySort : ""
+      const categoryOption = req.query.categorySort || ""
+      let CategoryOptionObj
+      // trans to Obj to match
+      if (categoryOption) {
+        CategoryOptionObj = mongoose.Types.ObjectId(req.query.categorySort)
+      }
+      const yearOption = req.query.yearSort || ""
       const categories = await Category.find().sort({ _id: 'asc' }).lean()
-      let record
-      if (categoryId) {
-        const searchKey = { categoryId }
-        record = await Record.find({ $and: [{ userId }, searchKey] }).populate('categoryId').lean()
-      }
-      else {
-        record = await Record.find({ userId }).populate('categoryId').lean()
-      }
+
+      let records = await Record.aggregate(
+        [
+          {
+            $project: {
+              id: 1,
+              name: 1,
+              categoryId: 1,
+              date: 1,
+              amount: 1,
+              year: { $year: '$date' },
+              month: { $month: '$date' },
+              userId: 1
+            },
+          },
+          {
+            $match: {
+              userId,
+              categoryId: CategoryOptionObj ? CategoryOptionObj : String,
+              year: yearOption ? Number(yearOption) : Number,
+            }
+          },
+          {
+            $lookup: {
+              from: 'categories',
+              localField: 'categoryId',
+              foreignField: '_id',
+              as: 'category'
+            }
+          },
+          { $sort: { date: -1 } }
+        ]
+      )
       let totalAmount = 0
-      // 轉換類別、時間
+      // 類別 selected
       categories.forEach(item => {
-        if (item._id.toString() === categoryId) {
+        if (item._id.toString() === categoryOption) {
           item.selected = 'selected'
         }
       })
-      for (let item of record) {
+      let yearList = new Set()
+      for (let item of records) {
+        yearList = yearList.add(new Date(item.date).getFullYear())
         item.date = moment(item.date).format('YYYY/MM/DD')
         totalAmount += item.amount
       }
-      res.render('index', { record, totalAmount, categories })
+      res.render('index', { records, totalAmount, categories, yearList })
     } catch (err) {
       next(err)
     }
